1c63aa47151286f57d5bca8c7936e8c79665a20b
T.Y.G -Team <103077384+mr-sami-x@users.noreply.github.com>
2022-07-30 18:47:07 +0000

    Add files via upload

---
+++ ip.py
@@ -0,0 +1,24 @@
+import os
+os.system('clear')
+import socket
+print("""
+\033[1;31m
+ ▄    ▄                ▄▄▄▄                  ▀   
+ ██  ██  ▄ ▄▄         █▀   ▀  ▄▄▄   ▄▄▄▄▄  ▄▄▄   
+ █ ██ █  █▀  ▀        ▀█▄▄▄  ▀   █  █ █ █    █   
+ █ ▀▀ █  █                ▀█ ▄▀▀▀█  █ █ █    █   
+ █    █  █            ▀▄▄▄█▀ ▀▄▄▀█  █ █ █  ▄▄█▄▄ 
+\033[1;32m							 
+#############################################
+\033[1;31mAuthor: \033[1;32mMr Sami Yemeni Hackers
+\033[1;31mGitHub: \033[1;32mhttps://github.com/mr-sami-x
+\033[1;31mFacebook: \033[1;32m T.Y.G TEAM
+#############################################
+	""")
+spider = input('\033[1;31mEnter URL Site :\033[1;32m ')
+sami = socket.gethostbyname(spider)
+print('-------------------')
+print('\033[1;31murl website : \033[1;32m', spider)
+print('\033[1;31mip website : \033[1;32m', sami)
+print('-------------------')
+print('\033[1;31mChannel tele :\033[1;32m @TYG_TEAM')
